4e9f6e5c6bda49a795b40134877c2734641992f2
001ZTG744 <naveen123.pathuri@gmail.com>
2021-07-27 16:52:29 +0530

    view

---
+++ src/app/employee.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private baseURL = 'http://localhost:8081/api/v1/employees';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getEmployeesList(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+  }
+  // tslint:disable-next-line:ban-types
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`, employee);
+  }
+
+  getEmployeeById(id: number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+}
